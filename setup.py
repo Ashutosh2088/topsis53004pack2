eaafb15096667ffcf15c863606b94058715101c3
Ashutosh2088 <52885679+Ashutosh2088@users.noreply.github.com>
2020-01-20 21:05:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
-setup(name='topsis53004pack',
+setup(name='topsis53004pack1',
 version='0.1',
 description='Library that implements the "The Technique for Order of Preference by Similarity to Ideal Solution (TOPSIS)"',
-url='https://github.com/Ashutosh2088/topsis53004pack',
-download_url='https://github.com/Ashutosh2088/topsis53004pack/archive/v0.1.tar.gz',
+url='https://github.com/Ashutosh2088/topsis53004pack1',
+download_url='https://github.com/Ashutosh2088/topsis53004pack1/archive/v0.1.tar.gz',
 author='Ashutosh Rattan',
 author_email='ashutosh544@gmail.com',
 license='MIT',
-packages=['topsis53004pack'],
+packages=['topsis53004pack1'],
 install_requires = ['numpy','pandas','math','sys'],
 zip_safe=False,
 classifiers=[
